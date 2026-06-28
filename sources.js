f6c28f53d8b3681323a706da33ffb4edc0c6adba
farhantaskinismail1 <ismailaarr8@gmail.com>
2026-06-28 22:46:30 +0600

    Update sources.js

--- sources.js
+++ sources.js
@@ -1,167 +1,266 @@
+const _0x215a70 = _0x45ec;
+function _0x5f01() {
+  const _0x495b59 = [
+    'WIN\x20SPORTS',
+    '222',
+    'logos/cart',
+    'thub.com/a',
+    'https://ex',
+    '5560wPieTo',
+    '10415290ywDepx',
+    '11QaZuXf',
+    's\x203',
+    'st/sony-te',
+    'FIFA',
+    '4833200KTMHHg',
+    'https://ip',
+    ':80/559/in',
+    'V/main/pla',
+    'Bangla',
+    'ample.com/',
+    '807831DHoWkQ',
+    'y-BDIX-IPT',
+    'BDIX\x20IPTV',
+    '237914DoyyBr',
+    'com/abusae',
+    'http://145',
+    'dex.m3u8',
+    '38IjSWht',
+    '.239.5.177',
+    'https://in',
+    'logos/jamu',
+    'Kids',
+    'Entertainm',
+    'https://ra',
+    'ournaments',
+    '1685YhSCVQ',
+    'n-1.m3u8',
+    'sets.footb',
+    'v/language',
+    'na.png',
+    'Fifa',
+    'cc/logos/t',
+    'ercontent.',
+    'logos/ekus',
+    'ylist.m3u',
+    '/1500x1500',
+    'T-IPTV/blo',
+    'e0b37b.png',
+    'Fifa+',
+    'd-cup-2026',
+    'A.m3u',
+    'eidx/Mrgif',
+    'BDIX\x20IPTV\x20',
+    'https://gi',
+    '48417CUbash',
+    'hub.io/ipt',
+    '--white.10',
+    'hey.png',
+    'Sony\x20Ten+',
+    'proviszon.',
+    '26873DwBNYs',
+    '333',
+    'oon-networ',
+    'b/main/FIF',
+    'ent',
+    'https://as',
+    '546niELyd',
+    '/fifa-worl',
+    's/ben.m3u',
+    'all-logos.',
+    'k.png',
+    'tv-org.git',
+    'han443/FAS',
+    'w.githubus',
+    'Bein\x20Sport',
+    'Indian',
+  ];
+  _0x5f01 = function () {
+    return _0x495b59;
+  };
+  return _0x5f01();
+}
+(function (_0x20652d, _0x316d00) {
+  const _0x4808a3 = _0x45ec,
+    _0x596ee4 = _0x20652d();
+  while (!![]) {
+    try {
+      const _0x58efad =
+        parseInt(_0x4808a3(0x19e)) / (-0x3be + -0x71 * 0x4a + -0xef * -0x27) +
+        (parseInt(_0x4808a3(0x1a2)) / (-0x259 + -0xf4e * 0x1 + 0x11a9)) *
+          (parseInt(_0x4808a3(0x174)) /
+            (0xab5 * 0x3 + -0x10e7 * -0x1 + -0x3103 * 0x1)) +
+        (-parseInt(_0x4808a3(0x18f)) / (0x1ff3 + -0x1ed8 + -0x5d * 0x3)) *
+          (-parseInt(_0x4808a3(0x1aa)) /
+            (-0x844 + -0xd3b * 0x1 + 0x36 * 0x66)) +
+        (parseInt(_0x4808a3(0x180)) /
+          (0x131 * 0xb + -0xca6 * 0x1 + 0x3 * -0x25)) *
+          (-parseInt(_0x4808a3(0x17a)) / (0x281 * 0x4 + -0x22ea + 0x18ed)) +
+        parseInt(_0x4808a3(0x195)) / (0x13d4 + -0xf * 0x192 + -0x1e1 * -0x2) +
+        parseInt(_0x4808a3(0x19b)) / (0x79b + -0x7dc * 0x4 + -0x5e * -0x41) +
+        (parseInt(_0x4808a3(0x190)) /
+          (-0x127 * 0x2 + -0x1 * 0x127d + -0x14d5 * -0x1)) *
+          (-parseInt(_0x4808a3(0x191)) / (0x1c3 + -0x54 * 0x2 + 0x88 * -0x2));
+      if (_0x58efad === _0x316d00) break;
+      else _0x596ee4['push'](_0x596ee4['shift']());
+    } catch (_0x22d228) {
+      _0x596ee4['push'](_0x596ee4['shift']());
+    }
+  }
+})(_0x5f01, 0x1e073 + -0x3cb * 0x11e + 0xc1 * 0x987);
+function _0x45ec(_0x5c7d58, _0x4b561a) {
+  _0x5c7d58 = _0x5c7d58 - (0x51 * 0x6b + 0xa04 * 0x2 + -0x3473);
+  const _0x3db316 = _0x5f01();
+  let _0x16e5aa = _0x3db316[_0x5c7d58];
+  return _0x16e5aa;
+}
 const IPTV_SOURCES = {
-  playlists: [
-    {
-      name: 'BDIX IPTV',
-      url: 'https://raw.githubusercontent.com/abusaeeidx/Mrgify-BDIX-IPTV/main/playlist.m3u',
-    },
-    {
-      name: 'BDIX IPTV 222',
-      url: 'https://github.com/ahan443/FAST-IPTV/blob/main/FIFA.m3u',
-    },
-    {
-      name: 'BDIX IPTV 333',
-      url: 'https://iptv-org.github.io/iptv/languages/ben.m3u',
-    },
-    // {
-    //   name: 'BDIX IPTV 4444444444',
-    //   url: 'https://iptv-org.github.io/iptv/categories/sports.m3u',
-    // },
-    // {
-    //   name: 'BDIX IPTV 5555555555',
-    //   url: 'https://www.scribd.com/document/789677819/New.m3u8',
-    // },
-  ],
-
-  channels: [
-    {
-      name: 'Bein Sports 3',
-      url: 'http://145.239.5.177:80/559/index.m3u8',
-      category: 'Fifa',
-      group: 'Fifa',
-      logo: 'https://assets.football-logos.cc/logos/tournaments/1500x1500/fifa-world-cup-2026--white.10e0b37b.png',
-    },
-    {
-      name: 'Sony Ten+',
-      url: 'https://inproviszon.st/sony-ten-1.m3u8',
-      category: 'Kids',
-      group: 'Fifa',
-      logo: 'https://assets.football-logos.cc/logos/tournaments/1500x1500/fifa-world-cup-2026--white.10e0b37b.png',
-    },
-    // {
-    //   name: 'SSSSSSSSSSSSSSSSSSS+',
-    //   url: 'https://inproviszon.st/sony-ten-1.m3u8',
-    //   category: 'Fifa',
-    //   group: 'Fifa',
-    //   logo: 'https://assets.football-logos.cc/logos/tournaments/1500x1500/fifa-world-cup-2026--white.10e0b37b.png',
-    // },
-    // Example manual channels:
-    // {
-    //   name: "Somoy TV",
-    //   url: "https://example.com/somoy.m3u8",
-    //   category: "News",
-    //   group: "Bangla TV",
-    //   logo: "https://example.com/somoy.png"
-    // }
-  ],
-};
-
-/*
-    Playlist/manual channel যেগুলোর category তুমি fixed করতে চাও,
-    এখানে channel name -> category map করে দাও।
-  
-    Allowed categories:
-    - Bangla
-    - FIFA
-    - Kids
-    - Global
-    - News
-    - Indian
-    - Entertainment
-  */
-const CATEGORY_RULES = {
-  // Bangla
-  'Aakaash Aath': 'Bangla',
-  'Aakaash Aath (576p)': 'Bangla',
-
-  'Aamar Bangla': 'Bangla',
-  'Aamar Bangla (720p)': 'Bangla',
-
-  'ATN Bangla': 'Bangla',
-  'ATN Bangla UK (576p)': 'Bangla',
-
-  'ATN News': 'Bangla',
-
-  'Bangla Vision': 'Bangla',
-  'Boishakhi TV': 'Bangla',
-
-  BTV: 'Bangla',
-  'BTV National (1080p)': 'Bangla',
-
-  'Channel i': 'Bangla',
-  'Channel I HD': 'Bangla',
-
-  'Channel 9 HD': 'Bangla',
-
-  'Channel 24': 'Bangla',
-  'Channel 24 HD': 'Bangla',
-
-  'DBC News': 'Bangla',
-  'DBC News HD': 'Bangla',
-
-  'Deepto TV': 'Bangla',
-  'Deepto TV HD': 'Bangla',
-
-  'Desh TV': 'Bangla',
-  'Deshe Bideshe': 'Bangla',
-
-  'Ekushey TV': 'Bangla',
-
-  'Ekattor HD': 'Bangla',
-  'Ekattor TV (1080p)': 'Bangla',
-
-  'Green TV (1080p)': 'Bangla',
-
-  'Independent TV': 'Bangla',
-
-  'Jamuna TV': 'Bangla',
-
-  'Maasranga TV': 'Bangla',
-
-  'Mohona TV (1080p)': 'Bangla',
-
-  'News 24 HD': 'Bangla',
-
-  NTV: 'Bangla',
-  RTV: 'Bangla',
-
-  'Somoy TV': 'Bangla',
-
-  // Sports / FIFA
-  'T Sports': 'FIFA',
-  'Sony Sports Ten 1': 'FIFA',
-  'Sony Sports Ten 2': 'FIFA',
-  'Sony Sports Ten 3': 'FIFA',
-  'Star Sports 1': 'FIFA',
-  'Star Sports 2': 'FIFA',
-
-  // Kids
-  'Cartoon Network': 'Kids',
-  Pogo: 'Kids',
-  Nick: 'Kids',
-  'Disney Channel': 'Kids',
-
-  // Indian
-  'Star Jalsha': 'Indian',
-  'Zee Bangla': 'Indian',
-  'Sony Aath': 'Indian',
-  'Colors Bangla': 'Indian',
-
-  // Entertainment
-  HBO: 'Entertainment',
-  'Sony Max': 'Entertainment',
-  'Star Gold': 'Entertainment',
-  'Zee Cinema': 'Entertainment',
-};
-// const BLOCKED_CHANNELS = [
-//   'Ekushey TV',
-//   'Jamuna TV',
-//   'Cartoon Network',
-//   'Bein Sports',
-//   'Bein Sports 1',
-// ];
-const CUSTOM_LOGOS = {
-  'Ekushey TV': 'https://example.com/logos/ekushey.png',
-  'Jamuna TV': 'https://example.com/logos/jamuna.png',
-  'Cartoon Network': 'https://example.com/logos/cartoon-network.png',
-};
-const PRIORITY_CHANNELS = ['WIN SPORTS', 'Bein Sports 3', 'Fifa+'];
+    playlists: [
+      {
+        name: _0x215a70(0x19d),
+        url:
+          _0x215a70(0x1a8) +
+          _0x215a70(0x187) +
+          _0x215a70(0x1b1) +
+          _0x215a70(0x19f) +
+          _0x215a70(0x171) +
+          _0x215a70(0x19c) +
+          _0x215a70(0x198) +
+          _0x215a70(0x1b3),
+      },
+      {
+        name: _0x215a70(0x172) + _0x215a70(0x18b),
+        url:
+          _0x215a70(0x173) +
+          _0x215a70(0x18d) +
+          _0x215a70(0x186) +
+          _0x215a70(0x1b5) +
+          _0x215a70(0x17d) +
+          _0x215a70(0x170),
+      },
+      {
+        name: _0x215a70(0x172) + _0x215a70(0x17b),
+        url:
+          _0x215a70(0x196) +
+          _0x215a70(0x185) +
+          _0x215a70(0x175) +
+          _0x215a70(0x1ad) +
+          _0x215a70(0x182),
+      },
+    ],
+    channels: [
+      {
+        name: _0x215a70(0x188) + _0x215a70(0x192),
+        url:
+          _0x215a70(0x1a0) +
+          _0x215a70(0x1a3) +
+          _0x215a70(0x197) +
+          _0x215a70(0x1a1),
+        category: _0x215a70(0x1af),
+        group: _0x215a70(0x1af),
+        logo:
+          _0x215a70(0x17f) +
+          _0x215a70(0x1ac) +
+          _0x215a70(0x183) +
+          _0x215a70(0x1b0) +
+          _0x215a70(0x1a9) +
+          _0x215a70(0x1b4) +
+          _0x215a70(0x181) +
+          _0x215a70(0x1b8) +
+          _0x215a70(0x176) +
+          _0x215a70(0x1b6),
+      },
+      {
+        name: _0x215a70(0x178),
+        url:
+          _0x215a70(0x1a4) +
+          _0x215a70(0x179) +
+          _0x215a70(0x193) +
+          _0x215a70(0x1ab),
+        category: _0x215a70(0x1a6),
+        group: _0x215a70(0x1af),
+        logo:
+          _0x215a70(0x17f) +
+          _0x215a70(0x1ac) +
+          _0x215a70(0x183) +
+          _0x215a70(0x1b0) +
+          _0x215a70(0x1a9) +
+          _0x215a70(0x1b4) +
+          _0x215a70(0x181) +
+          _0x215a70(0x1b8) +
+          _0x215a70(0x176) +
+          _0x215a70(0x1b6),
+      },
+    ],
+  },
+  CATEGORY_RULES = {
+    'Aakaash\x20Aath': _0x215a70(0x199),
+    'Aakaash\x20Aath\x20(576p)': _0x215a70(0x199),
+    'Aamar\x20Bangla': _0x215a70(0x199),
+    'Aamar\x20Bangla\x20(720p)': _0x215a70(0x199),
+    'ATN\x20Bangla': _0x215a70(0x199),
+    'ATN\x20Bangla\x20UK\x20(576p)': _0x215a70(0x199),
+    'ATN\x20News': _0x215a70(0x199),
+    'Bangla\x20Vision': _0x215a70(0x199),
+    'Boishakhi\x20TV': _0x215a70(0x199),
+    BTV: _0x215a70(0x199),
+    'BTV\x20National\x20(1080p)': _0x215a70(0x199),
+    'Channel\x20i': _0x215a70(0x199),
+    'Channel\x20I\x20HD': _0x215a70(0x199),
+    'Channel\x209\x20HD': _0x215a70(0x199),
+    'Channel\x2024': _0x215a70(0x199),
+    'Channel\x2024\x20HD': _0x215a70(0x199),
+    'DBC\x20News': _0x215a70(0x199),
+    'DBC\x20News\x20HD': _0x215a70(0x199),
+    'Deepto\x20TV': _0x215a70(0x199),
+    'Deepto\x20TV\x20HD': _0x215a70(0x199),
+    'Desh\x20TV': _0x215a70(0x199),
+    'Deshe\x20Bideshe': _0x215a70(0x199),
+    'Ekushey\x20TV': _0x215a70(0x199),
+    'Ekattor\x20HD': _0x215a70(0x199),
+    'Ekattor\x20TV\x20(1080p)': _0x215a70(0x199),
+    'Green\x20TV\x20(1080p)': _0x215a70(0x199),
+    'Independent\x20TV': _0x215a70(0x199),
+    'Jamuna\x20TV': _0x215a70(0x199),
+    'Maasranga\x20TV': _0x215a70(0x199),
+    'Mohona\x20TV\x20(1080p)': _0x215a70(0x199),
+    'News\x2024\x20HD': _0x215a70(0x199),
+    NTV: _0x215a70(0x199),
+    RTV: _0x215a70(0x199),
+    'Somoy\x20TV': _0x215a70(0x199),
+    'T\x20Sports': _0x215a70(0x194),
+    'Sony\x20Sports\x20Ten\x201': _0x215a70(0x194),
+    'Sony\x20Sports\x20Ten\x202': _0x215a70(0x194),
+    'Sony\x20Sports\x20Ten\x203': _0x215a70(0x194),
+    'Star\x20Sports\x201': _0x215a70(0x194),
+    'Star\x20Sports\x202': _0x215a70(0x194),
+    'Cartoon\x20Network': _0x215a70(0x1a6),
+    Pogo: _0x215a70(0x1a6),
+    Nick: _0x215a70(0x1a6),
+    'Disney\x20Channel': _0x215a70(0x1a6),
+    'Star\x20Jalsha': _0x215a70(0x189),
+    'Zee\x20Bangla': _0x215a70(0x189),
+    'Sony\x20Aath': _0x215a70(0x189),
+    'Colors\x20Bangla': _0x215a70(0x189),
+    HBO: _0x215a70(0x1a7) + _0x215a70(0x17e),
+    'Sony\x20Max': _0x215a70(0x1a7) + _0x215a70(0x17e),
+    'Star\x20Gold': _0x215a70(0x1a7) + _0x215a70(0x17e),
+    'Zee\x20Cinema': _0x215a70(0x1a7) + _0x215a70(0x17e),
+  },
+  CUSTOM_LOGOS = {
+    'Ekushey\x20TV':
+      _0x215a70(0x18e) + _0x215a70(0x19a) + _0x215a70(0x1b2) + _0x215a70(0x177),
+    'Jamuna\x20TV':
+      _0x215a70(0x18e) + _0x215a70(0x19a) + _0x215a70(0x1a5) + _0x215a70(0x1ae),
+    'Cartoon\x20Network':
+      _0x215a70(0x18e) +
+      _0x215a70(0x19a) +
+      _0x215a70(0x18c) +
+      _0x215a70(0x17c) +
+      _0x215a70(0x184),
+  },
+  PRIORITY_CHANNELS = [
+    _0x215a70(0x18a),
+    _0x215a70(0x188) + _0x215a70(0x192),
+    _0x215a70(0x1b7),
+  ];
